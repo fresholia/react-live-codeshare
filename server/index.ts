b589732c702a8cae37afba0c0276335d67d9106a
cleopatra <enesakilliok17@gmail.com>
2021-11-19 00:20:05 +0300

    Update code

--- server/index.ts
+++ server/index.ts
@@ -24,57 +24,57 @@ interface CodeControllerType {
 
 const rooms: CodeControllerType = {}
 
-nextApp.prepare().then(async() => {
-    const socketApp: Express = app;
-    const server: http.Server = http.createServer(socketApp);
-    const io: socketio.Server = new socketio.Server();
+const socketApp: Express = app;
+const server: http.Server = http.createServer(socketApp);
+const io: socketio.Server = new socketio.Server();
 
-    io.attach(server);
+io.attach(server);
 
-    io.on('connection', (client: socketio.Socket) => {
-        client.on('disconnect', () => {
-            const room = client.handshake.auth.room;
-            if (room && rooms[room]) {
-                rooms[room].removeClient(client.id)
-            }
+io.on('connection', (client: socketio.Socket) => {
+    client.on('disconnect', () => {
+        const room = client.handshake.auth.room;
+        if (room && rooms[room]) {
+            rooms[room].removeClient(client.id)
+        }
+    })
+
+    client.on('code.setRoom', (room: string, name: string) => {
+        client.join(room);
+        client.handshake.auth.room = room
+
+        if (!rooms[room]) {
+            rooms[room] = new CodeController(room)
+        }
+
+        rooms[room].getContent((data: ICodeBlocks) => {
+            const ipaddr = client.handshake.address
+            rooms[room].addClient(client.id, name, ipaddr)
+
+            data.clientId = client.id
+
+            client.emit('code.get', data, ipaddr, rooms[room].getClients())
         })
+    })
 
-        client.on('code.setRoom', (room: string, name: string) => {
-            client.join(room);
-            client.handshake.auth.room = room
+    client.on('code.sync', (room: string, content: string[], codePosition: ICodeBlocks) => {
+        if (rooms[room]) {
+            rooms[room].setCodeContent(content)
+            rooms[room].setClientData(client.id, codePosition)
+        }
 
-            if (!rooms[room]) {
-                rooms[room] = new CodeController(room)
-            }
+        io.to(room).emit('code.update', content, rooms[room].getClients())
+    })
 
-            rooms[room].getContent((data: ICodeBlocks) => {
-                const ipaddr = client.handshake.address
-                rooms[room].addClient(client.id, name, ipaddr)
-
-                data.clientId = client.id
-
-                client.emit('code.get', data, ipaddr, rooms[room].getClients())
-            })
-        })
-
-        client.on('code.sync', (room: string, content: string[], codePosition: ICodeBlocks) => {
-            if (rooms[room]) {
-                rooms[room].setCodeContent(content)
-                rooms[room].setClientData(client.id, codePosition)
-            }
-
-            io.to(room).emit('code.update', content, rooms[room].getClients())
-        })
-
-        client.on('code.updateData', (room: string, data: string, value: string | number | string[]) => {
-            if (rooms[room]) {
-                rooms[room].updateCache(data, value)
-            }
-        })
-    });
-
-    socketApp.all('*', (req: any, res: any) => nextHandler(req, res));
-    server.listen(port);
+    client.on('code.updateData', (room: string, data: string, value: string | number | string[]) => {
+        if (rooms[room]) {
+            rooms[room].updateCache(data, value)
+        }
+    })
 });
+
+socketApp.all('*', (req: any, res: any) => nextHandler(req, res));
+server.listen(port);
+
+nextApp.prepare().then();
 
 export default rooms
