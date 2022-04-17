9df8d0758ccc3b0aa2814044fb695851eb87b598
cleopatra <enesakilliok17@gmail.com>
2022-04-18 01:01:06 +0300

    Add serverSideProps

--- src/pages/[...codeDetails].tsx
+++ src/pages/[...codeDetails].tsx
@@ -1,10 +1,11 @@
+import type { GetServerSideProps } from 'next';
 import { NextPageWithLayout } from 'types/definitions';
 
 import Header from 'components/header';
 import CodeLayout from 'layouts/code';
 
 const Code: NextPageWithLayout = () => {
-  return <></>;
+  return <>asdasdasd</>;
 };
 
 Code.getLayout = (page) => {
@@ -14,6 +15,23 @@ Code.getLayout = (page) => {
       {page}
     </CodeLayout>
   );
+};
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const {
+    query: { codeDetails },
+    res,
+  } = context;
+
+  const code: string = codeDetails ? codeDetails[0] : ('' as string);
+
+  if (code != '') {
+    // Request the server and retrieve details about the code snippet
+  }
+
+  return {
+    props: {},
+  };
 };
 
 export default Code;
